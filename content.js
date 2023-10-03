61fce7e6ffcf96c5764484853292ce2cfd57f2ea
sigubrat <sigurd.brattland@fangstr.no>
2023-10-03 10:29:30 +0200

    WIP: Observe page for mutations to hopefully optimize performance

--- content.js
+++ content.js
@@ -1,10 +1,42 @@
-// If the popup window is open, close it when the page loads.
-// This will only occur on youtube.com pages
+// Start observing the document with the configured parameters
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.type === 'childList') {
+        console.log("[DEBUG] Mutation detected, checking for specific element...");
+        const specificElement = document.querySelector('your-element-selector');
+        if (specificElement) {
+          console.log("[DEBUG] Specific element found, starting new observer...");
+          observer.disconnect(); // stop observing the document
+          
+          // Start observing the specific element
+          const newObserver = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+              if (mutation.type === 'childList') {
+                console.log("[DEBUG] Mutation detected, checking for popup...");
+                const dismissButton = document.getElementById('dismiss-button');
+                if (dismissButton) {
+                  dismissButton.click();
+                  dismissButton.remove(); // removes the popup from the DOM
+                  console.log('[DEBUG] DISGUSTING UGLY POPUP WAS CLOSED HELLO NOTICE ME HELLO IT WORKED HOPEFULLY');
+          
+                  const playButton = document.querySelector('button.ytp-play-button');
+                  if (playButton) {
+                    console.log("[DEBUG] Play button found, resuming video...");
+                    playButton.click(); // resumes the video
+                  } else {
+                    console.log("[DEBUG] Play button not found, cannot resume video.");
+                  }
+                } else {
+                  console.log("[DEBUG] Dismiss button not found, cannot close popup.");
+                }
+              }
+            });
+          });
 
-window.addEventListener('load', function() {
-    const dismissButton = document.getElementById('dismiss-button');
-    if (dismissButton) {
-      dismissButton.click();
-      console.log('[DEBUG] Popup window closed');
-    }
-  });
+          newObserver.observe(specificElement, { childList: true, subtree: true });
+        }
+      }
+    });
+});
+
+observer.observe(document, { childList: true, subtree: true });
