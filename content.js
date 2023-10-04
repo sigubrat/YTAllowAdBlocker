3e83b2aca3f84f7253d306dee0307cd6ca379116
Sigubrat <Sigurdjbratt@hotmail.no>
2023-10-04 12:46:24 +0200

    Testing suggests functional prototype

--- content.js
+++ content.js
@@ -1,10 +1,25 @@
 // If the popup window is open, close it when the page loads.
 // This will only occur on youtube.com pages
 
-window.addEventListener('load', function() {
+// Function to check for the dismiss button and click it
+function checkAndClickButton() {
     const dismissButton = document.getElementById('dismiss-button');
     if (dismissButton) {
-      dismissButton.click();
-      console.log('[DEBUG] Popup window closed');
+        dismissButton.click();
+        dismissButton.remove();
+        console.log('[DEBUG] Popup window closed');
+        
+        const playButton = document.querySelector('button.ytp-play-button');
+        if (playButton) {
+            console.log("[DEBUG] Play button found, resuming video...");
+            playButton.click(); // resumes the video
+        }
+        
+        // Clear the interval once we've found and clicked the button
+        clearInterval(intervalId);
     }
-  });
+}
+
+// Set up a polling interval to check for the button every 1000ms
+const intervalId = setInterval(checkAndClickButton, 1000);
+
